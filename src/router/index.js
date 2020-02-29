664c2acb0d3efdc2b0d354a5383034d26fdbfe8a
foan888 <foan888/outlook.com>
2020-02-29 15:48:06 +0800

    c初始化项目-整理目录-准备资源” git commit -m 始化项目-整理目录-准备资源”

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+import Home from '@/views/home'// 简写模式替换原有方式
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    pash: '/',
+    redirect: '/home' // 强制跳转到home页面
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    name: 'Home',
+    component: Home
   }
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@
 
 <script>
 // @ is an alias to /src
+// @代表一个别名 /src的别名
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
