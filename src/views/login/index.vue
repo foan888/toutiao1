66d6d27747f9d18c044186bdfc892394055a25b3
foan888 <foan888/outlook.com>
2020-03-02 10:13:16 +0800

    登录模块-数据绑定及校验-表单校验准备

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,23 +7,23 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt="">
       </div>
-      <!-- 表单 -->
-      <el-form style="margin-top:20px">
-          <!-- 表单容器 -->
-          <el-form-item>
-              <!-- 表单域 -->
-              <el-input placeholder="请输入手机号"></el-input>
+      <!-- 表单 绑定model属性 绑定rules属性（表单验证规则）-->
+      <el-form  :model="loginForm" :rules="loginRules" style="margin-top:20px">
+          <!-- 表单容器 设置prop属性 prop表示要校验的字段名-->
+          <el-form-item prop="mobile">
+              <!-- 表单域 v-model双向绑定-->
+              <el-input v-model="loginForm.mible" placeholder="请输入手机号"></el-input>
           </el-form-item>
           <!-- 验证码 -->
-          <el-form-item>
-              <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+          <el-form-item prop="code">
+              <el-input v-model="loginForm.code" style="width:60%" placeholder="请输入验证码"></el-input>
               <!-- 放按钮 -->
               <el-button style="float:right" plain>发送验证码</el-button>
           </el-form-item>
           <!-- 表单域 -->
-          <el-form-item>
-              <!-- 霸王条款 -->
-              <el-checkbox>我已阅读同意用户协议和隐私条款</el-checkbox>
+          <el-form-item prop="checked">
+              <!-- 霸王条款 是否同意被坑 -->
+              <el-checkbox v-model="loginForm.checked">我已阅读同意用户协议和隐私条款</el-checkbox>
           </el-form-item>
           <!-- 按钮 -->
           <el-form-item>
@@ -36,7 +36,20 @@
 
 <script>
 export default {
+  data () {
+    return {
+      // 登录表单的数据
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false // 是否同意用户协议
+      },
+      // 定义表单的验证规则
+      loginRules: {
 
+      }
+    }
+  }
 }
 </script>
 
